0815d4dfe5551154db90bec74ada9e2100883238
Collin <braeuning.collin@gmail.com>
2026-07-24 22:51:32 -0400

    fix table sizing on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const SAMPLES = Object.entries(sampleUrls).map(([path, url]) => ({
   name: path.split('/').pop()!.replace(/\.[^/.]+$/, ''),
     url,
 }))
+.sort((a, b) => b.name.localCompare(a.name)) // more recent dates float to top
 
 interface FileData {
   fileName: string
